3e15f6be40dfc147b0ddd29f0e795327e817f3ec
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-08-20 15:19:53 +0300

    Users view

--- blogs-front/src/components/Users.js
+++ blogs-front/src/components/Users.js
@@ -1,3 +1,32 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { initializeUsers } from "../reducers/usersReducer";
+
 export const Users = () => {
-  return <>Users</>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(initializeUsers());
+  }, []);
+  const users = useSelector((state) => state.users);
+
+  return (
+    <>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th> <th>Blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((u) => (
+            <tr key={u.id}>
+              <td>{u.username}</td>
+              <td>{u.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
---
+++ blogs-front/src/reducers/usersReducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import userService from "../services/users";
+
+export const initializeUsers = () => {
+  return async (dispatch) => {
+    const users = await userService.getAll();
+    dispatch(setUsers(users));
+  };
+};
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: [],
+  reducers: {
+    setUsers(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setUsers } = usersSlice.actions;
+export default usersSlice.reducer;
